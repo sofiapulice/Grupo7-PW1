4bacd925634e74f5d193bdae1b34c5a223e48f24
Pablo Barcena <pabloandresbarcena@gmail.com>
2024-06-21 17:56:42 -0300

    Con JS hice que se vea el trailer al hacer clic en el boton ver trailer

---
+++ Proyecto/Peliculas/detalle.js
@@ -0,0 +1,6 @@
+let mostrarTrailer = document.getElementById("verTrailer");
+let mostrarIframe = document.querySelector("iframe");
+
+mostrarTrailer.addEventListener("click", ()=> {
+        mostrarIframe.style.display = "block";
+});
