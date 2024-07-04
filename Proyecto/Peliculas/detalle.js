2b7ba13c655e77805ed46ea1d2fc28ee1c370a72
sofiapulice <165835429+sofiapulice@users.noreply.github.com>
2024-07-04 17:58:19 -0300

    Update detalle.js

--- Proyecto/Peliculas/detalle.js
+++ Proyecto/Peliculas/detalle.js
@@ -1,3 +1,32 @@
+const episodiosPorTemporada = {
+        temporada1: 8,
+        temporada2: 10,
+        temporada3: 8,
+        temporada4: 9,
+        temporada5: 5,
+        temporada6: 6,
+        temporada7: 10,
+        temporada8: 7,
+        temporada9: 8,
+    };
+    
+    function cargarCapitulos() {
+        const temporadaSeleccionada = document.getElementById("temporadaSelect").value;
+        const cantidadEpisodios = episodiosPorTemporada[temporadaSeleccionada];
+    
+        const episodioSelect = document.getElementById("episodioSelect");
+        episodioSelect.innerHTML = "";
+    
+        for (let i = 1; i <= cantidadEpisodios; i++) {
+            const opcionEpisodio = document.createElement("option");
+            opcionEpisodio.text = `Episodio ${i}`;
+            opcionEpisodio.value = `episodio${i}`;
+            episodioSelect.appendChild(opcionEpisodio);
+        }
+    }
+    
+    cargarCapitulos();
+
 let mostrarTrailer = document.getElementById("verTrailer");
 let mostrarIframe = document.querySelector("iframe");
 
